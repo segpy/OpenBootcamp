663f8f28b8e06f956809d4af6acbd166bd529caf
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-08-19 10:35:04 -0500

    Ejercicio 3 introduccion

---
+++ Introduccion/Coche.java
@@ -0,0 +1,23 @@
+package Introduccion;
+
+public class Coche {
+    public int puertas;
+
+    public Coche(int puertas) {
+        this.puertas = puertas;
+    }
+
+    public void incrementarPuertas(){
+        this.puertas++;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+
+
+}
--- Introduccion/ejercicio_tema3.java
+++ Introduccion/ejercicio_tema3.java
@@ -2,6 +2,26 @@ package Introduccion;
 
 public class ejercicio_tema3 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio tema 3");
+        System.out.println("Primera parte: ");
+        System.out.println(ejercicio1(5, 10, 15)+"\n");
+    
+
+        System.out.println("Segunda parte: ");
+        Coche miCoche = new Coche(5);
+        System.out.println("El coche tiene " + miCoche.getPuertas() + " puertas");
+        miCoche.setPuertas(10);
+        System.out.println("Ahora el coche tiene " + miCoche.getPuertas() + " puertas");
+        System.out.println("Incrementando el numero de puertas...");
+        miCoche.incrementarPuertas();
+        System.out.println("El coche tiene " + miCoche.getPuertas() + " puertas");
+
+
     }
+
+    public static int ejercicio1(int a, int b, int c){
+        return a + b + c;
+    }
+
 }
+
+
