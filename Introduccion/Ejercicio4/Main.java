e21c19657db2edd18fa0797b38e2641c8e74e9b4
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-08-19 20:05:37 -0500

    Ejercicio4 corregido visualmente

--- Introduccion/Ejercicio4/Main.java
+++ Introduccion/Ejercicio4/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Primera parte: ");
-        System.out.println("Intoduzca el numerolf: ");
+        System.out.println("Primera parte ");
+        System.out.print("Intoduzca el numerolf: ");
         int numero = sc.nextInt();
         if(numero > 0){
             System.out.println("El numero es positivo");
